600ec96e16af77ac606f83aeaab85f45c1b108ed
Caleb Salyards <caleb.salyards@outlook.com>
2023-05-17 00:39:59 -0600

    Node now connects to SQL

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 
+const mysqlClient = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+  });
+
+mysqlClient.connect((error) => {
+    if (error) throw error;
+    console.log("Connected to MySQL Server");
+});
